9532b278ecb418e049be7ef283d94eb47026d36b
Omkar Desai <odesai@paypal.com>
2023-03-17 14:01:14 -0700

    :rocket: Add client build to git

---
+++ client/build/assets/PageLayout-f2d47461.js
@@ -0,0 +1,1 @@
+import{b as l,c as r,F as n,j as a,N as c,L as i,O as o}from"./index-dda91878.js";const p=t=>{const s=l();return r(n,{children:[a("h3",{children:t.label}),a("hr",{}),a(c,{onClick:s,children:t.links.map(e=>a(i,{to:e.path,label:e.label,onClick:s},e.path))}),a(o,{})]})};export{p as default};
