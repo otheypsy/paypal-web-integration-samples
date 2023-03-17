331192fde0cf37b464aac8c8578880e34426eb7a
Omkar Desai <odesai@paypal.com>
2023-03-17 13:32:36 -0700

    :bug: Fix client asset routing

--- server/routes/main.router.js
+++ server/routes/main.router.js
@@ -1,17 +1,16 @@
 import express from 'express'
-import clientController from '../controllers/client.controller.js'
+import { client, assets } from '../controllers/client.controller.js'
 import btRouter from './braintree.router.js'
 import testbedRouter from './testbed.router.js'
 import notFoundMiddleware from '../middleware/notFound.middleware.js'
 import responderMiddleware from '../middleware/responder.middleware.js'
 import errorMiddleware from '../middleware/error.middleware.js'
+import { resolve } from 'path'
 
+const root = resolve('./client/build')
 const router = express.Router()
 
 // Pre-logic router-level middleware
-
-// React Front-End
-router.use('/', clientController)
 
 // API entry-point
 router.use('/api', (req, res, next) => {
@@ -30,6 +29,10 @@ router.use('/api', (req, res, next) => {
 router.use('/api/braintree', btRouter)
 // mainRouter.use('/api/paypal/rest', paypal)
 router.use('/api/testbed', testbedRouter)
+
+// React Front-End
+router.use(assets)
+router.use('/', client)
 
 // Post-logic router-level middleware
 router.use(responderMiddleware)
